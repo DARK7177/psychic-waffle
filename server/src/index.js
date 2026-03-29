e54e29f871a60a1452c4f34b3f3ec7890d10058a
Bhupesh <Bhupesh5676l@gmail.com>
2026-03-29 21:41:28 +0530

    Updated Enpoint

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,10 @@ const teacherRoutes = require('./routes/teacherRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(errorHandler);
 app.use('/api/auth', authRoutes)
@@ -29,7 +32,7 @@ app.get('/', (req, res) => {
     res.send("QR Attendance Backend Running")
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`)
